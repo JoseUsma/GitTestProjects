3c92b386a10d4fd37dce399bc23e351da2139904
JoseUsma <jmusma@hotmail.com>
2013-08-13 17:12:24 -0700

    Added more project samples (using NetBeans 7.3.1)

---
+++ HelloSpring/web/WEB-INF/jsp/helloView.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : helloView
+    Created on : Aug 13, 2013, 4:19:15 PM
+    Author     : Jose
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hello</title>
+    </head>
+    <body>
+        <h1>${helloMessage}</h1>
+    </body>
+</html>
---
+++ HelloWeb/src/java/org/mypackage/hello/NameHanlder.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.hello;
+
+/**
+ *
+ * @author Jose
+ */
+public class NameHanlder {
+    private String name;
+    public NameHanlder(){
+    name = null;
+    };
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
